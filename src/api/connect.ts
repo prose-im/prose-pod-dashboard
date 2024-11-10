2c0794fd74eefda3564467266e801d37870fb9ae
Paloma <karpaloma@hotmail.com>
2024-11-10 14:28:52 +0100

    fix: add connection file

---
+++ src/api/connect.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+class Connect {
+  /**  CONFIG  **/ 
+
+  async initializeAccount(): Promise<void> {
+    await axios.put(`http://127.0.0.1:8000/v1/init/first-account`, 
+      {
+        "username": "remi.bardon",
+        "password": "HsV>dFx^P[m!jM+4W9wf5k:_t{8pSQ*?",
+        "nickname": "Rémi B."
+      }
+    ); 
+  }
+  
+  async setPodAddess(): Promise<void> {
+    await axios.put(`http://127.0.0.1:8000/v1/pod/config/address`),
+    {
+      "hostname": "crisp.chat"
+    }
+  }
+
+  async initializeServer(): Promise<void> {
+    await axios.put(`http://127.0.0.1:8000/v1/server/config`),
+    {
+      "domain": "crisp.chat"
+    }
+  }
+
+  async initializeServer(): Promise<void> {
+    await axios.put(`http://127.0.0.1:8000/v1/server/config`),
+    {
+      "domain": "crisp.chat"
+    }
+  }
+}
+
+export default new Connect;
