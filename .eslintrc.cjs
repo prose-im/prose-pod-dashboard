44aa0aa1e94de81618d955146363e0b7412e92b1
Valerian Saliou <valerian@valeriansaliou.name>
2024-11-10 15:01:03 +0100

    chore: bootstrap common project structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    node: true
+  },
+
+  parser: "vue-eslint-parser",
+
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  },
+
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier"
+  ],
+
+  plugins: ["@typescript-eslint"],
+  rules: {}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,95 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+/*
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ */
+
+/**************************************************************************
+ * IMPORTS
+ * ************************************************************************* */
+
+import fs from "fs";
+import path from "path";
+import merge from "lodash.merge";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+
+import commonConfig from "./config/common";
+import developmentConfig from "./config/development";
+import productionConfig from "./config/production";
+
+/**************************************************************************
+ * INTERFACES
+ * ************************************************************************* */
+
+interface Configuration {
+  environment: string;
+}
+
+/**************************************************************************
+ * CONSTANTS
+ * ************************************************************************* */
 
 const ASSETS_ICONS_PATH = path.join(__dirname, "src/assets/icons/");
 
-// https://vitejs.dev/config/
+/**************************************************************************
+ * CONFIGURATION
+ * ************************************************************************* */
+
+const CONFIG: Configuration = (function () {
+  // Initialize empty configuration
+  const config = {
+    environment: ""
+  };
+
+  // Merge common configuration
+  merge(config, commonConfig);
+
+  // Merge per-environment configuration
+  switch (process.env.NODE_ENV) {
+    case "production": {
+      merge(config, productionConfig);
+
+      break;
+    }
+
+    default: {
+      merge(config, developmentConfig);
+    }
+  }
+
+  // Merge local configuration? (if any)
+  try {
+    const localConfig = JSON.parse(
+      fs.readFileSync("./config/local.json", "utf8")
+    );
+
+    merge(config, localConfig);
+  } catch (_) {
+    // Ignore errors (local configuration not found)
+  }
+
+  return config;
+})();
+
+/**************************************************************************
+ * EXPORTS
+ * ************************************************************************* */
+
 export default defineConfig({
   resolve: {
-    alias: [
-      { find: "@/", replacement: path.join(__dirname, "src/") },
-    ]
+    alias: [{ find: "@/", replacement: path.join(__dirname, "src/") }]
   },
 
   plugins: [
     vue(),
+
     createSvgIconsPlugin({
       iconDirs: [ASSETS_ICONS_PATH],
       symbolId: "icon-[name]",
       inject: "body-last",
-      customDomId: "__svg__icons__",
-      // svgoOptions: false
+      customDomId: "__svg__icons__"
     })
   ],
 
@@ -35,8 +104,10 @@ export default defineConfig({
       }
     },
 
-    postcss: {
-    }
+    postcss: {}
   },
-  
-})
+
+  define: {
+    __CONFIG__: CONFIG
+  }
+});
