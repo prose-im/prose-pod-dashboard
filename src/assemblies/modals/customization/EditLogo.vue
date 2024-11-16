17f268c05db5943dda5264a254ba9b5a117f3af5
Paloma <karpaloma@hotmail.com>
2024-11-16 12:15:00 +0100

    fix: add store  bound edit name modal, store bound prototype for edit logo and create non functional  edit detail card file

---
+++ src/assemblies/modals/customization/EditLogo.vue
@@ -0,0 +1,213 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+  TEMPLATE
+  ********************************************************************** -->
+
+<template lang="pug">
+base-modal(
+  position="center"
+  title="Select new icon"
+  buttonColor="purple"
+  buttonLabel="Change icon"
+  @close="onClose"
+  @confirm="onProceed"
+)
+  .a-edit-logo
+    h4
+      | New icon
+
+    .a-edit-logo__upload
+      base-avatar(
+        :avatarDataUrl="imageUrl"
+        class="a-edit-logo__upload--avatar"
+        size="60px"
+        borderRadius="7px"
+      )
+
+      base-button(
+        tint="white"
+      )
+        | Upload image...
+
+      input(
+        type="file"
+        class="a-edit-logo__input"
+        ref="fileInput"
+        accept="image/*"
+        @change="onFilePicked"
+      )
+</template>
+  
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// PROJECT: COMPONENTS
+import BaseAvatar from '@/components/base/BaseAvatar.vue';
+import BaseButton from '@/components/base/BaseButton.vue';
+import BaseIcon from '@/components/base/BaseIcon.vue';
+import BaseModal from '@/components/base/modal/BaseModal.vue';
+import FormField from '@/components/form/FormField.vue';
+import store from '@/store';
+
+export default {
+  name: "AddCustomEmoji",
+
+  components: {
+    BaseAvatar,
+    BaseButton,
+    BaseIcon,
+    BaseModal,
+    FormField
+  },
+
+  props: {
+
+  },
+
+  emits: ["close", "proceed"],
+
+  data() {
+    return {
+      // --> STATE <--
+
+      imageUrl: '',
+
+      image: '',
+
+      shortcut: ''
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {
+    // --> HELPERS <--
+    onPickFile () {
+      this.$refs.fileInput.click()
+    },
+
+    onFilePicked (event) {
+      console.log('hi');
+      const files = event.target.files
+      let filename = files[0].name
+      const fileReader = new FileReader()
+      fileReader.addEventListener('load', () => {
+        this.imageUrl = fileReader.result
+      })
+      fileReader.readAsDataURL(files[0])
+      this.image = files[0]
+    },
+
+    onProceed () {
+      const date = new Date()
+      const options = { 
+        day: 'numeric', 
+        month: 'long', 
+        year: 'numeric' 
+      };
+
+      const formattedDate = new Intl.DateTimeFormat('en-GB', options).format(date);
+
+      const newReaction = {
+        id: "e8f6dbac-cda3-460e-88f1-5e588c64c76e",
+        imageUrl: this.imageUrl,
+        shortcut: this.shortcut,
+        date: formattedDate,
+        contributor: 'Valerian Saliou',
+        contributorAvatar: "https://avatars.githubusercontent.com/u/1451907?v=4"
+      }
+
+      store.$customizationEmojis.addReaction(newReaction);
+      
+      this.imageUrl = '';
+      this.image = '';
+      this.$emit('close');
+    },
+
+    onClose () {
+      this.imageUrl = '';
+      this.image = '';
+      this.$emit('close');
+    }
+
+  },
+
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".a-edit-logo";
+
+#{$c} {
+  margin-inline: 48px;
+  font-family: $font-family-default;
+
+  h4{
+    color:$color-text-secondary;
+    margin-top: 0;
+    margin-bottom: 11px;
+    margin-left: 8px;
+    font-weight: $font-weight-medium;
+  }
+
+  #{$c}__upload{
+    display: flex;
+    align-items: center;
+    position: relative;
+    padding-block: 13.5px;
+    padding-left: 33px;
+    margin-bottom: 30px;
+    border: 1px solid $color-border-secondary;
+    border-radius: 7px;
+
+    &--avatar{
+      outline: 1px solid $color-border-secondary;
+      outline-offset: 1.5px;
+      margin-right: 21.5px;
+    }
+  }
+
+  #{$c}__input{
+    position: absolute;
+    cursor: pointer;
+    width: 120px;
+    left: 18%;
+    opacity: 0;
+  }
+
+  #{$c}__info{
+    display: flex;
+    align-items: center;
+    font-weight: $font-weight-light;
+    margin-top: 38px;
+    margin-left: 9px;
+    font-size: ($font-size-baseline - 1px);
+
+    p{
+      margin: 0;
+    }
+
+    &--icon{
+      margin-right: 13px;
+    }
+  }
+  
+}
+
+</style>
+            
---
+++ src/assemblies/modals/customization/EditName.vue
@@ -0,0 +1,109 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+  TEMPLATE
+  ********************************************************************** -->
+
+<template lang="pug">
+base-modal(
+  position="center"
+  title="Change your workspace name"
+  buttonColor="purple"
+  buttonLabel="Change name"
+  @close="onClose"
+  @confirm="onProceed"
+)
+  .a-edit-name
+    base-modal-input-block(
+      v-model="newName"
+      label="New name"
+      placeholder="ex: iCorp"
+    )
+</template>
+  
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// PROJECT: COMPONENTS
+import BaseAvatar from '@/components/base/BaseAvatar.vue';
+import BaseButton from '@/components/base/BaseButton.vue';
+import BaseIcon from '@/components/base/BaseIcon.vue';
+import BaseModal from '@/components/base/modal/BaseModal.vue';
+import BaseModalInputBlock from '@/components/base/modal/BaseModalInputBlock.vue';
+import FormField from '@/components/form/FormField.vue';
+import store from '@/store';
+
+export default {
+  name: "AddCustomEmoji",
+
+  components: {
+    BaseAvatar,
+    BaseButton,
+    BaseIcon,
+    BaseModal,
+    BaseModalInputBlock,
+    FormField
+  },
+
+  props: {
+
+  },
+
+  emits: ["close", "proceed"],
+
+  data() {
+    return {
+      // --> STATE <--
+      newName: ''
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {
+    // --> HELPERS <--
+
+    onProceed () {
+      store.$customizationWorkspace.updateWorkspaceName(this.newName);
+      
+      this.newName ='';
+      this.$emit('close');
+    },
+
+    onClose () {
+
+      this.newName = '';
+      this.$emit('close');
+    }
+
+  },
+
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".a-edit-name";
+
+#{$c} {
+  margin-inline: 48px;
+  margin-block-end: 30px;
+  font-family: $font-family-default;
+  
+}
+
+</style>
+            
