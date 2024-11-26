5133946bbe1c5fb9cbf0a19cc42ff0b3c75cddd5
Paloma <karpaloma@hotmail.com>
2024-11-26 22:08:15 +0100

    feture: create skeleton loader for network checks

---
+++ src/components/base/loader/LoaderNetworkCheckRow.vue
@@ -0,0 +1,111 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+<template lang="pug">
+.c-loader-network-check-row(
+class="c-loader-network-check-row--flex"
+)
+  base-pulse-icon(
+    class="c-loader-network-check-row__icon"
+  )     
+
+  .c-loader-network-check-row__right(
+    :class=`[
+      "c-loader-network-check-row--flex",
+    ]`
+  )
+    .c-loader-network-check-row__loading(
+      class="c-loader-network-check-row__animated"
+    )
+
+    p.c-loader-network-check-row__status
+      | Pending
+</template>
+
+<!-- **********************************************************************
+      SCRIPT
+      ********************************************************************** -->
+
+<script lang="ts">
+import BasePulseIcon from "@/components/base/BasePulseIcon.vue";
+
+export default {
+  name: "LoaderNetworkCheckRow",
+
+  components: {
+    BasePulseIcon,
+  },
+
+  props: {},
+
+  data() {
+    return {
+      // --> STATE <--
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {
+    // <-- HELPERS -->
+  },
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-loader-network-check-row";
+
+#{$c} {
+  margin-bottom: 9px;
+
+  #{$c}__icon {
+    margin-right: 9px;
+  }
+
+  #{$c}__loading {
+    width: 180px;
+    height: 18px;
+  }
+
+  #{$c}__animated {
+    border-radius: 7px;
+    background: linear-gradient(90deg, #0001 33%, #0005 50%, #0001 66%) #f2f2f2;
+    background-size: 300% 100%;
+    animation: l1 1s infinite linear;
+  }
+
+  #{$c}__right {
+    flex: 1;
+    justify-content: space-between;
+  }
+
+  #{$c}__status {
+    color: $color-base-blue-normal;
+    font-size: ($font-size-baseline - 2px);
+  }
+
+  &--flex {
+    display: flex;
+    align-items: center;
+  }
+
+  @keyframes l1 {
+    0% {
+      background-position: right;
+    }
+  }
+}
+</style>
+<!-- -->
