b24db269d0cd762b3a9891ea3ca75544da10acae
Paloma <karpaloma@hotmail.com>
2024-12-02 14:26:35 -0300

    fix: format

--- src/views/app/dashboard/customization/AppCustomizationEmojis.vue
+++ src/views/app/dashboard/customization/AppCustomizationEmojis.vue
@@ -13,73 +13,72 @@
   emojis-reactions-dashboard
 
 </template>
-  
+
 <!-- **********************************************************************
      SCRIPT
      ********************************************************************** -->
 
 <script lang="ts">
 // PROJECT: COMPONENTS
-import EmojisReactionsDashboard from '@/components/emojis-reactions/EmojisReactionsDashboard.vue';
+import EmojisReactionsDashboard from "@/components/emojis-reactions/EmojisReactionsDashboard.vue";
 
 export default {
   name: "AppCustomizationEmojis",
 
   components: {
-    EmojisReactionsDashboard
+    EmojisReactionsDashboard,
   },
 
-  props: {
-
-  },
+  props: {},
 
   emits: [],
 
   data() {
     return {
       // --> STATE <--
-      messagingItems:[
+      messagingItems: [
         {
-          subtitle:"Store archives of all messages",
-          description: "Archives are required for users running Prose apps on multiple devices, so that previous messages synchronize across all devices. End-to-end encrypted messages are stored as-is",
-          type:"toggle"
+          subtitle: "Store archives of all messages",
+          description:
+            "Archives are required for users running Prose apps on multiple devices, so that previous messages synchronize across all devices. End-to-end encrypted messages are stored as-is",
+          type: "toggle",
         },
         {
           subtitle: "Message archive retention time",
-          description: "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
-          type:"button",
-          typeProps:{
-            label:"Edit details...",
-            size:"medium"
-          }
+          description:
+            "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
+          type: "button",
+          typeProps: {
+            label: "Edit details...",
+            size: "medium",
+          },
         },
-                {
+        {
           subtitle: "Message archive retention time",
-          description: "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
-          type:"select",
-          typeProps:{
-            options:[
+          description:
+            "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
+          type: "select",
+          typeProps: {
+            options: [
               {
-                icon:"",
-                label:"1 year"
-              }, 
+                icon: "",
+                label: "1 year",
+              },
               {
-                icon:"",
-                label:"2 years"
-              }
+                icon: "",
+                label: "2 years",
+              },
             ],
-            size:"medium"
-          }
-        }
-      ]
+            size: "medium",
+          },
+        },
+      ],
     };
   },
 
   computed: {},
 
   watch: {},
-
-  created() {},
 
   methods: {
     // --> HELPERS <--
--- src/views/app/dashboard/team/AppTeamMembers.vue
+++ src/views/app/dashboard/team/AppTeamMembers.vue
@@ -15,14 +15,14 @@
   )
 
 </template>
-  
+
 <!-- **********************************************************************
      SCRIPT
      ********************************************************************** -->
 
 <script lang="ts">
 // PROJECT: COMPONENTS
-import MembersInvitesDashboard from '@/components/members-invites/MembersInvitesDashboard.vue';
+import MembersInvitesDashboard from "@/components/members-invites/MembersInvitesDashboard.vue";
 
 export default {
   name: "AppTeamMembers",
@@ -31,57 +31,56 @@ export default {
     MembersInvitesDashboard,
   },
 
-  props: {
-
-  },
+  props: {},
 
   emits: [],
 
   data() {
     return {
       // --> STATE <--
-      messagingItems:[
+      messagingItems: [
         {
-          subtitle:"Store archives of all messages",
-          description: "Archives are required for users running Prose apps on multiple devices, so that previous messages synchronize across all devices. End-to-end encrypted messages are stored as-is",
-          type:"toggle"
+          subtitle: "Store archives of all messages",
+          description:
+            "Archives are required for users running Prose apps on multiple devices, so that previous messages synchronize across all devices. End-to-end encrypted messages are stored as-is",
+          type: "toggle",
         },
         {
           subtitle: "Message archive retention time",
-          description: "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
-          type:"button",
-          typeProps:{
-            label:"Edit details...",
-            size:"medium"
-          }
+          description:
+            "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
+          type: "button",
+          typeProps: {
+            label: "Edit details...",
+            size: "medium",
+          },
         },
-                {
+        {
           subtitle: "Message archive retention time",
-          description: "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
-          type:"select",
-          typeProps:{
-            options:[
+          description:
+            "Messages from the archive can be automatically expunged after some time. This is a good privacy practice, as it can be considered unsafe to retain all past messages in storage.",
+          type: "select",
+          typeProps: {
+            options: [
               {
-                icon:"",
-                label:"1 year"
-              }, 
+                icon: "",
+                label: "1 year",
+              },
               {
-                icon:"",
-                label:"2 years"
-              }
+                icon: "",
+                label: "2 years",
+              },
             ],
-            size:"medium"
-          }
-        }
-      ]
+            size: "medium",
+          },
+        },
+      ],
     };
   },
 
   computed: {},
 
   watch: {},
-
-  created() {},
 
   methods: {
     // --> HELPERS <--
