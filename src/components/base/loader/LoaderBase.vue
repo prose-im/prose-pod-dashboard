b5501696891d9e8870670b2c2da546160802baf1
Paloma <karpaloma@hotmail.com>
2024-12-03 10:31:52 -0300

    feature: create base for skeleton loader

---
+++ src/components/base/loader/LoaderBase.vue
@@ -0,0 +1,76 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+<template lang="pug">
+.c-loader-base
+  .c-loader-base__animated(
+    :style=`{
+      width: width,
+    }`
+  )
+</template>
+
+<!-- **********************************************************************
+      SCRIPT
+      ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "LoaderBase",
+
+  components: {},
+
+  props: {
+    width: {
+      type: String,
+      default: "100px",
+    },
+  },
+
+  data() {
+    return {
+      // --> STATE <--
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {
+    // <-- HELPERS -->
+  },
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-loader-base";
+
+#{$c} {
+  margin-bottom: 5.5px;
+
+  #{$c}__animated {
+    height: 13.5px;
+    border-radius: 7px;
+    background: linear-gradient(90deg, #0001 33%, #0005 50%, #0001 66%) #f2f2f2;
+    background-size: 300% 100%;
+    animation: l1 1s infinite linear;
+  }
+
+  @keyframes l1 {
+    0% {
+      background-position: right;
+    }
+  }
+}
+</style>
