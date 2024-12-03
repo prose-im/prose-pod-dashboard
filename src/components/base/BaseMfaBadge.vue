e9dd6354b217f4a355129fa0696379e59aacafc4
Paloma <karpaloma@hotmail.com>
2024-12-03 10:41:54 -0300

    feature: create a specific badge for MfA

---
+++ src/components/base/BaseMfaBadge.vue
@@ -0,0 +1,79 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+ TEMPLATE
+ ********************************************************************** -->
+
+<template lang="pug">
+.c-base-mfa-badge
+  base-icon(
+    class="c-base-mfa-badge__icon"
+    name="padlock"
+    fill="#05c02b"
+    size="16px"
+  )
+
+  p(
+    class="c-base-mfa-badge--light"
+  )
+    | Enabled
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+import BaseIcon from "./BaseIcon.vue";
+
+export default {
+  name: "BaseMfaBadge",
+
+  components: {
+    BaseIcon,
+  },
+
+  props: {
+    size: {
+      type: String,
+      default: "short",
+    },
+  },
+
+  data() {
+    return {
+      // --> STATE <--
+    };
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {},
+};
+</script>
+
+<!-- **********************************************************************
+  STYLE
+  ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-mfa-badge";
+
+#{$c} {
+  display: flex;
+  align-items: center;
+  font-size: ($font-size-baseline - 2px);
+  color: $color-base-green-normal;
+  font-weight: $font-weight-light;
+
+  #{$c}__icon {
+    margin-right: 6.5px;
+  }
+}
+</style>
