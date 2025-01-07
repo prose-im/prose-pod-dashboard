1c987a8ab6dfab1551f8d32b3c190f01418ac5f2
Paloma <karpaloma@hotmail.com>
2025-01-07 13:25:06 -0300

    feature: create a notification tooltip

---
+++ src/components/base/BaseNotification.vue
@@ -0,0 +1,82 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+  TEMPLATE
+  ********************************************************************** -->
+
+<template lang="pug">
+.c-base-notification
+  p
+    | Sucess
+
+  base-icon(
+    class="c-base-notification__icon"
+    name="checkmark.circle.fill"
+    fill="#05c02b"
+    size="12px"
+  )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// PROJECT: COMPONENTS
+
+// STORE
+
+// PACKAGES
+
+export default {
+  name: "BaseNotification",
+
+  props: {
+    visibility: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  emits: [],
+
+  data() {
+    return {
+      // --> STATE <--
+    };
+  },
+
+  computed: {},
+
+  methods: {
+    // --> HELPERS <--
+    // --> EVENT LISTENERS <--
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-notification";
+
+#{$c} {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  top: 5%;
+  right: 2%;
+  padding-right: 10px;
+  font-size: $font-size-baseline;
+
+  #{$c}__icon {
+    padding-left: 4px;
+  }
+}
+</style>
