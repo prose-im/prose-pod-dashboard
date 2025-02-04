e66ad77febb4ab84b23eb1fadb4ecab328832b92
Paloma <karpaloma@hotmail.com>
2025-02-04 12:04:19 -0600

    feature: create sign-up form generic block

--- src/components/signup/SignupForm.vue
+++ src/components/signup/SignupForm.vue
@@ -10,17 +10,40 @@
 
 <template lang="pug">
 .c-signup-form
-  p.c-signup-form__label
-    | {{ label }}
+  .c-signup-form__label
+    slot
 
-  .c-signup-form__field-block
+  form.c-signup-form__field-block(
+    @submit.prevent="onClick"
+    :class=`[
+      {
+        "c-signup-form__field-block--block" : formType === 'double'
+      }
+    ]`
+  )
     form-field(
       v-model="input"
       class="c-signup-form__field"
+      autofocus
+      align="left"
+      :placeholder="placeholder"
       size="ultra-large"
+      :type="type"
+    )
+
+    form-field(
+      v-if="formType === 'double'"
+      v-model="secondInput"
+      align="left"
+      class="c-signup-form__field"
+      :placeholder="secondaryPlaceholder"
+      size="ultra-large"
+      :type="secondaryType"
     )
 
     base-button(
+      @click="onClick"
+      class="c-signup-form__button"
       tint="purple"
       size="ultra-large"
     )
@@ -42,25 +65,80 @@ export default {
   name: "SignupPage",
 
   props: {
-    label: {
-      type: String,
-      required: true
-    },
-
     buttonLabel: {
       type: String,
-      default: "Continue"
-    }
+      default: "Continue",
+    },
+
+    formType: {
+      type: String,
+      default: "single",
+    },
+
+    modelValue: {
+      type: String,
+      default: "",
+    },
+
+    secondaryInput: {
+      type: String,
+      default: "",
+    },
+
+    placeholder: {
+      type: String,
+      default: "",
+    },
+
+    secondaryPlaceholder: {
+      type: String,
+      default: "",
+    },
+
+    type: {
+      type: String,
+      required: true,
+    },
+
+    secondaryType: {
+      type: String,
+      default: "",
+    },
   },
+
+  emits: ["update:modelValue", "updateSecondInput", "changeStep"],
 
   data() {
     return {
       // --> STATE <--
-      input: ""
     };
   },
 
-  computed: {}
+  computed: {
+    input: {
+      get() {
+        return this.modelValue;
+      },
+      set(value: string) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+
+    secondInput: {
+      get() {
+        return this.secondaryInput;
+      },
+      set(value: string) {
+        this.$emit("updateSecondInput", value);
+      },
+    },
+  },
+
+  methods: {
+    onClick() {
+      this.$emit("changeStep");
+    },
+  },
 };
 </script>
 
@@ -83,12 +161,21 @@ $c: ".c-signup-form";
   #{$c}__field-block {
     display: flex;
     flex: 1 1 auto;
+
+    &--block {
+      display: block;
+
+      #{$c}__button {
+        margin-block-start: 20px;
+      }
+    }
   }
 
   #{$c}__field {
     display: flex;
     flex: 1 1 auto;
     margin-inline-end: 20px;
+    margin-block-end: 12px;
   }
 
   @media (max-width: 768px) {
