0d896a8dda51d29dc599c626dc744367bb376bf0
Rémi Bardon <remi@remibardon.name>
2025-04-09 14:21:01 +0200

    feat: Check password before Factory reset

--- src/assemblies/modals/advanced/FactoryReset.vue
+++ src/assemblies/modals/advanced/FactoryReset.vue
@@ -64,6 +64,17 @@ import BaseAlert from "@/components/base/BaseAlert.vue";
 // PROJECT: STORE
 import store from "@/store";
 
+// PROJECT: API
+import APIAuth from "@/api/providers/auth";
+
+function initialState() {
+  return {
+    hasDownloadedBackup: false,
+    acceptsDataLoss: false,
+    password: ""
+  };
+}
+
 export default {
   name: "FactoryReset",
 
@@ -77,14 +88,7 @@ export default {
   emits: ["close"],
 
   data() {
-    return {
-      // --> STATE <--
-
-      hasDownloadedBackup: false,
-      acceptsDataLoss: false,
-
-      password: ""
-    };
+    return initialState();
   },
 
   computed: {},
@@ -97,23 +101,29 @@ export default {
       // Check if the whole form was filled
       // FIXME: Check `hasDownloadedBackup` once [Export full backup · Issue #131 · prose-im/prose-pod-api](https://github.com/prose-im/prose-pod-api/issues/131) is fixed.
       if (!this.password) {
-        BaseAlert.error("Please enter your password");
+        return BaseAlert.error("Please enter your password");
       } else if (!this.acceptsDataLoss) {
-        BaseAlert.error(
+        return BaseAlert.error(
           "Please confirm that you have read and do accept all the conditions"
         );
-      } else {
-        // Reset state
-        this.password = "";
-
-        this.hasDownloadedBackup = false;
-        this.acceptsDataLoss = false;
-
-        await store.$globalConfig.performFactoryReset();
-        // WORKAROUND: Wait a bit so the user is logged out
-        //   and the refresh does not redirect to `/team/members`.
-        setTimeout(() => (window.location.href = "/"), 50);
+      } else if (!store.$account.$state.session.jid) {
+        return BaseAlert.error("Internal error: Can’t find your JID");
       }
+
+      const jid = store.$account.$state.session.jid;
+      try {
+        await APIAuth.login(jid, this.password);
+      } catch (e: any) {
+        return BaseAlert.error("Invalid password");
+      }
+
+      // Reset state
+      Object.assign(this.$data, initialState());
+
+      await store.$globalConfig.performFactoryReset();
+      // WORKAROUND: Wait a bit so the user is logged out
+      //   and the refresh does not redirect to `/team/members`.
+      setTimeout(() => (window.location.href = "/"), 50);
     }
   }
 };
