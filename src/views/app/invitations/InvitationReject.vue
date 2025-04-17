a02aa646de4335206195d4fb68cad1e130448946
Paloma <karpaloma@hotmail.com>
2025-04-17 20:18:06 +0200

    style: improved style

--- src/views/app/invitations/InvitationReject.vue
+++ src/views/app/invitations/InvitationReject.vue
@@ -16,17 +16,17 @@
     h1 
       | Cancelling this invitation…
 
-    p 
-      | All of your data will be erased.
+    p.v-invitation-reject__subtitle 
+      | You have rejected this invitation
 
   .v-invitation-reject__content(
     v-else-if="invitationCancelled === true"
   )
     h1 
-      | This invitation was cancelled by the person who send it.
+      | This invitation was cancelled by the person who send it
 
-    p 
-      | All the data associated to you has been erased.
+    p.v-invitation-reject__subtitle 
+      | Please contact the server Admin to get a new invite
 
   .v-invitation-reject__content(
     v-else
@@ -34,8 +34,8 @@
     h1 
       | ✋ This invitation has expired
 
-    p 
-      | Unless you already accepted this invitation before, all of your data has been erased.
+    p.v-invitation-reject__subtitle 
+      | Please contact the server Admin to get a new invite
 </template>
 
 <!-- **********************************************************************
@@ -88,6 +88,8 @@ $c: ".v-invitation-reject";
 #{$c} {
   height: 100%;
   width: 100%;
+  display: flex;
+  align-items: center;
   overflow: hidden;
   background: linear-gradient(
       rgba(255, 255, 255, 0.98),
@@ -95,6 +97,20 @@ $c: ".v-invitation-reject";
     ),
     url("/images/components/base/BaseTopography.svg");
   background-size: 35%;
+  text-align: center;
+
+  h1 {
+    font-size: ($font-size-page + 3px);
+    font-weight: $font-weight-medium;
+    margin-block: 0 17px;
+  }
+
+  #{$c}__subtitle {
+    font-size: ($font-size-page + 3px);
+    font-weight: $font-weight-light;
+    margin-block: 0 108px;
+    color: $color-base-grey-normal;
+  }
 
   #{$c}__content {
     max-width: 30em;
