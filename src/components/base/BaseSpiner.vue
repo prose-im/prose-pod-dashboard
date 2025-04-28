201c3398eb05b37be15a8c0e2345d050ef112fff
Paloma <karpaloma@hotmail.com>
2025-04-28 18:12:27 +0200

    feat: add a spiner component

---
+++ src/components/base/BaseSpiner.vue
@@ -0,0 +1,80 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+   TEMPLATE
+   ********************************************************************** -->
+
+<template lang="pug">
+.c-base-spiner
+  span.c-base-spiner__loader
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseSpiner"
+};
+</script>
+
+<!-- **********************************************************************
+  STYLE
+  ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-spiner";
+
+#{$c} {
+  #{$c}__loader {
+    position: absolute !important;
+    top: 60%;
+    left: 60%;
+    display: block;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    position: relative;
+    animation: rotate 1s linear infinite;
+  }
+
+  #{$c}__loader::before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    inset: 0px;
+    border-radius: 50%;
+    border: 4px solid $color-base-purple-normal;
+    animation: prixClipFix 2s linear infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes prixClipFix {
+    0% {
+      clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+    }
+    25% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+    }
+    50% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+    }
+    75% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+    }
+    100% {
+      clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+    }
+  }
+}
+</style>
