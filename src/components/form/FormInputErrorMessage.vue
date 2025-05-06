bedb3cfbee3384a16a80717ceb57cb5baba193fe
Paloma <karpaloma@hotmail.com>
2025-05-06 15:13:31 +0200

    feature: create a tag for Input error messages

---
+++ src/components/form/FormInputErrorMessage.vue
@@ -0,0 +1,59 @@
+<!--
+ * This file is part of prose-pod-dashboard
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+  TEMPLATE
+  ********************************************************************** -->
+
+<template lang="pug">
+.c-input-error-message
+  base-icon(
+    class="c-input-error-message__icon"
+    name="exclamationmark.triangle.fill"
+    fill="#dd2f2f"
+    size="12px"
+  )
+
+  span
+    slot
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseNotification",
+
+  props: {
+    visibility: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-input-error-message";
+
+#{$c} {
+  display: flex;
+  align-items: center;
+  padding-right: 10px;
+  font-size: $font-size-baseline;
+  color: $color-base-red-normal;
+
+  #{$c}__icon {
+    padding-inline-end: 4px;
+  }
+}
+</style>
