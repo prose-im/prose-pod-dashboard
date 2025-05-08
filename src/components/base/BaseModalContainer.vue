e396f5cac80d3412f7b55deb5d113f2e5759789b
Paloma <karpaloma@hotmail.com>
2025-05-08 21:17:54 +0200

    fix: rollback

--- src/components/base/BaseModalContainer.vue
+++ src/components/base/BaseModalContainer.vue
@@ -73,7 +73,7 @@ transition(
         span(
           :class=`[
             {
-              "c-base-modal-container--hidden": loading 
+              "c-base-modal-container--hidden": loading && disabled
             }
           ]`
         )
